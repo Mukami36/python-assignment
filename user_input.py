f0be86cda6203b467fecf693101c9a17be6e3904
Mukami36 <160488288+Mukami36@users.noreply.github.com>
2024-03-07 14:54:26 +0300

    first attempt

---
+++ user_input.py
@@ -0,0 +1,5 @@
+print('Hello')
+name = input('What is your name? ')
+age = int(input('What is your age? '))
+location = input('What is your location? ')
+f'Hello {name},you are {age} years old and live in {location}'
